bbfdb97b4be18f75b11f24d1ad7b8e69b2ea8298
franciscocruz29 <franciscohuertas25@gmail.com>
2023-01-25 11:30:16 -0500

    Finished sidebar and navbar

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
+// data imports
+import User from "./models/User.js";
+import { dataUser } from "./data/index.js";
+
 /* CONFIGURATION */
 dotenv.config();
 const app = express();
@@ -29,10 +33,13 @@ app.use("/sales", salesRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
-mongoose.set('strictQuery', false)
+mongoose.set('strictQuery', false);
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ONLY ADD DATA ONE TIME */
+    // User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} did not connect`));
